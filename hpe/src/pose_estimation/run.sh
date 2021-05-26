c3f8920f50b1d3b8e26b482821a69e47b5f704fa
de4d56 <denis.durasinovic@gmail.com>
2021-05-26 18:18:10 +0200

    Modified CMakeLists.txt, changed topic to hardware camera, bugfixes with CMakeLists

--- hpe/src/pose_estimation/run.sh
+++ hpe/src/pose_estimation/run.sh
@@ -1,1 +1,1 @@
-python pose_estimation/camera_hpe.py --cfg experiments/mpii/resnet152/384x384_d256x3_adam_lr1e-3.yaml --flip-test --model-file models/pytorch/pose_mpii/pose_resnet_152_384x384.pth.tar
+python pose_estimation/camera_hpe.py --cfg experiments/mpii/resnet50/384x384_d256x3_adam_lr1e-3.yaml --flip-test --model-file models/pose_resnet_50_384x384.pth.tar
