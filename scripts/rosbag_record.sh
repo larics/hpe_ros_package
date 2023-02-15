13f65611225fb4b87f9c44065cfa34b4e93f119c
Filip Zoric <fzoric8@gmail.com>
2023-02-15 20:15:25 +0100

    Added scripts

--- scripts/rosbag_record.sh
+++ scripts/rosbag_record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-bash -c "source /opt/ros/melodic/setup.bash; rosbag record /upper_body_3d"
+bash -c "source /opt/ros/melodic/setup.bash; rosbag record /upper_body_3d /right_arm /left_arm /cart_right_arm /cart_left_arm"
 
 
