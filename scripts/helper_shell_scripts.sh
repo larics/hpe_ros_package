ce45397694d64d0549df1eab67c43d3a07046ce0
Filip Zoric <fzoric8@gmail.com>
2023-05-02 15:17:01 +0200

    Added waitFor methodw

--- scripts/helper_shell_scripts.sh
+++ scripts/helper_shell_scripts.sh
@@ -4,3 +4,12 @@ waitForRealsenseCamera() {
     sleep 1; 
   done
 }
+
+waitFor() {
+  until timeout 3s rostopic echo $1 -n 1 --noarr > /dev/null 2>&1; do
+    echo "waiting for $1"
+    sleep 1;
+  done
+}
+
+
