d16bf3f58f53cc3bba4fdeb8369db14d72170c68
fzoric8@gmail.com <filip.zoric@fer.hr>
2024-11-29 17:30:57 +0000

    Added rosbag_record.sh

--- scripts/rosbag_record.sh
+++ scripts/rosbag_record.sh
@@ -1,5 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
 
-bash -c "source /opt/ros/melodic/setup.bash; rosbag record /upper_body_3d /right_arm /left_arm /cart_right_arm /cart_left_arm"
+# Define the topics to record
+topics=(
+    "/hand_2d"
+    "/hh_img"
+    "/hpe_2d"
+    "/joint_states"
+    "/leftw_point"
+    "/pose1"
+    "/pose2"
+    "/rectify_color/parameter_descriptions"
+    "/rectify_color/parameter_updates"
+    "/rightw_point"
+    "/rosout"
+    "/rosout_agg"
+    "/tf"
+    "/tf_static"
+    "/upper_body_3d"
+    "/vect1"
+    "/vect2"
+    "/vect3"
+    "/vect4"
+)
+
+# Start recording with the specified topics
+echo "Starting rosbag recording for selected topics..."
+rosbag record "${topics[@]}" -O topic2.bag
 
 
