97cad3298be0f319badd8cb5f3177418520beed5
Filip Zoric <filip.zoric@fer.hr>
2025-01-24 11:25:23 +0100

    Minor refactoring, decoupled utils, added vr script

--- src/utils.py
+++ src/utils.py
@@ -12,45 +12,7 @@ def limitCmd(cmd, upperLimit, lowerLimit):
         cmd = lowerLimit
     return cmd
 
-def arrayToVect(array, vect): 
-    vect.x = array[0]
-    vect.y = array[1]
-    vect.z = array[2]
-    return vect
-
-def arrayToPoint(array, point):
-    point.x = array[0]
-    point.y = array[1]
-    point.z = array[2]
-    return point
-
-def pointToArray(msg): 
-    return np.array([msg.x, msg.y, msg.z])
-
-# Create Rotation matrices 
-def get_RotX(angle):  
-    
-    RX = np.array([[1, 0, 0], 
-                   [0, np.cos(angle), -np.sin(angle)], 
-                   [0, np.sin(angle), np.cos(angle)]])
-    
-    return RX
-
-def get_RotY(angle): 
-    
-    RY = np.array([[np.cos(angle), 0, np.sin(angle)], 
-                   [0, 1, 0], 
-                   [-np.sin(angle), 0, np.cos(angle)]])
-    return RY
-    
-def get_RotZ(angle): 
-    
-    RZ = np.array([[np.cos(angle), -np.sin(angle), 0],
-                   [np.sin(angle), np.cos(angle), 0], 
-                   [ 0, 0, 1]] )
-    
-    return RZ
-
+# ROS msgs packers and unpackers
 def packTorsoPositionMsg(now, keypoints):
     msg = TorsoJointPositions()
     msg.header.stamp = now
@@ -322,14 +284,6 @@ def index_and_operation(A, keys, indices, operation):
     """
     A = get_allocation_matrix((len(keys), 3))
 
-def create_homogenous_vector(vector):
-    return np.append(vector, 1)
-
-def create_homogenous_matrix(R, t):
-    T_ = np.matrix(np.hstack((R, t.reshape(3, 1))))
-    T = np.matrix(np.vstack((T_, np.array([0, 0, 0, 1])))).round(5)
-    return T
-
 # Losing precision here [How to quantify lost precision here?]
 def resize_preds_on_original_size(preds, img_size):
     resized_preds = []
@@ -375,6 +329,7 @@ def getZeroTransform():
     t.rotation.w = 1
     return t
 
+# Publish marker array
 def publishMarkerArray(self, bD):
     mA = MarkerArray()
     i = 0
